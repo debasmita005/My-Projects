726c8b7783bffebdc3923a53133f76b474ec6cda
debasmita005 <debasmitasaha005@gmail.com>
2025-03-11 17:22:57 +0530

    Update polymorphism.java

--- Notes/oops/polymorphism.java
+++ Notes/oops/polymorphism.java
@@ -1,1 +1,36 @@
+// Parent class
+class Animal {
+    // Method to be overridden
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+// Child class
+class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Child class
+class Cat extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Animal animal;  // Reference of parent class
+
+        animal = new Dog();
+        animal.sound();  // Calls Dog's sound() method at runtime
+
+        animal = new Cat();
+        animal.sound();  // Calls Cat's sound() method at runtime
+    }
+}
 
